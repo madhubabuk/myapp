c7d936bc17bba8bae4ede33a64fcb451e7312ad1
babu7 <babu7uc@gmail.com>
2022-05-10 23:36:19 +0530

    first change

--- src/main/java/com/hub/myapp/MyUtil.java
+++ src/main/java/com/hub/myapp/MyUtil.java
@@ -7,6 +7,7 @@ import org.apache.tomcat.util.codec.binary.Base64;
 public class MyUtil {
 
 	public static void main(String[] args) {
+		System.out.println("here 1");
 		// TODO Auto-generated method stub
 		 String adminuserCredentials = "user:passwd";
 	        String encodedCredentials =
